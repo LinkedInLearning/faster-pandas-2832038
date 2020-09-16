670929895ea054b7aac7b7f01822da816aecec9c
Miki Tebeka <miki.tebeka@gmail.com>
2020-09-16 11:19:52 +0300

    Ch05_challenge recorded

---
+++ Ch05/challenge/dist_changes.py
@@ -0,0 +1,7 @@
+"""Calculate the minimal and maximal distance driven from the data at
+    taxi.csv.xz
+Consume as little memory as possible and don't load more than 50,000 rows at a
+time.
+"""
+
+import pandas as pd
